af26ff56eb71de7e77782b8da0b29ce3d9dd3df4
Patrick Knisely <patrick_knisely@hotmail.com>
2022-05-10 09:09:55 -0500

    Background gradient working, changes to black around depth of 700

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,20 +8,21 @@ public class BackgroundController : MonoBehaviour
     [SerializeField] Gradient gradient;
     [SerializeField] Color color;
     [SerializeField] PlayerController playerController;
+    [SerializeField] Camera camera;
     private float depth;
 
     // Start is called before the first frame update
     void Start()
     {
-        Camera.main.backgroundColor = color;
+ //       Camera.main.backgroundColor = color;
     }
 
     // Update is called once per frame
     void Update()
     {
         depth = playerController.GetDepth();
-        float value = Mathf.Lerp(0f, 1f, depth);
+        float value = Mathf.Lerp(0f, 1f, Math.Abs((depth/1000)));
         color = gradient.Evaluate(value);
-        Camera.main.backgroundColor = color;
+        camera.backgroundColor = color;
     }
 }
